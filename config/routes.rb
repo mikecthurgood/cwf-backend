9aa2f373d4b610a3ebcba4e32e5b1c67e5d30012
Mike Thurgood <mikecthurgood@gmail.com>
2020-01-08 23:10:40 +0000

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  get 'users/create', to: 'users/create'
-  get 'users/:id/update', to: 'users#update'
+  get 'users/create', to: 'users#create'
   get 'users', to: 'users#index'
-  get 'validate', to: 'users/validate' 
+  get 'validate', to: 'users#validate' 
   post 'signin', to: 'users#signin'
+  patch 'users/:id/update', to: 'users#update'
+  delete 'users/:id', to: 'users#delete'
+  
+  get 'reviews/:id', to: 'reviews#show'
+  get 'reviews', to: 'reviews#index'
+  post 'reviews', to: 'reviews#create'
+  patch 'reviews/:id', to: 'reviews#update'
+  delete 'reviews/:id', to: 'reviews#delete'
   
   get 'walls/:id', to: 'walls#show'
   get 'walls', to: 'walls#index'
