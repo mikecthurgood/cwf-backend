0bee5814de2b48e0a8fc426a74d6f98364343ed7
Mike Thurgood <mikecthurgood@gmail.com>
2020-01-11 11:59:09 +0000

    user login complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,34 +10,19 @@ class UsersController < ApplicationController
 
     if user && user.authenticate(params[:password])
       token = issue_token({id: user.id})
-      render json: { token: token, user: user.to_json() }
+      render json: { token: token, user: user }
     else
       render json: {error: 'Invalid credentials.'}, status: 401
     end
   end
-  # JS Code for React App
-
-  # signIn = user => {
-  #   this.setState({username: user.username})
-  #   localStorage.setItem('token', user.token)
-  # }
-
-  # signout = () => {
-  #   this.setState({username: ''})
-  #   localStorage.removeITem('token')
-  # }
-
-  # code for throwing error if user not found for JS side
-  # .then(data => {
-  #   if (data.error) throw Error(data.error)
-  # })
+  
 
   def validate
     user = get_current_user
 
     if user
       token = issue_token({id: user.id})
-      render json: { token: token, user: user.to_json() }      #{username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
+      render json: { token: token, user: user }      #{username: user.username, userSkills: user.skills, userActivities: user.activities, userSkillZaps: user.skill_zaps, token: issue_token({id: user.id})}
     else
       render json: {error: 'Unable to validate user.'}, status: 401
     end
